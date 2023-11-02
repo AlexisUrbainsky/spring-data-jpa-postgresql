14824d9dd7e87fee7d6aa70f064e116956819543
AlexisUrbainsky <aurbainsky@gmail.com>
2023-11-01 22:53:20 -0300

    first commit

---
+++ src/main/java/com/alexurba/springdatajpapostgresql/SpringDataJpaPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.alexurba.springdatajpapostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaPostgresqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexurba/springdatajpapostgresql/SpringDataJpaPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexurba.springdatajpapostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
